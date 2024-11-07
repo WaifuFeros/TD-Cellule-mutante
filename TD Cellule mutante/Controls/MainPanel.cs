25025f749d82e8080469ac4e3f3bcbfb0d16b79d
WaifuFeros <payet.pep@gmail.com>
2024-11-07 18:12:19 +0100

    Ajout MainPanel et SimulationButton

---
+++ TD Cellule mutante/Controls/MainPanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+namespace GraphiqueFromCode.Controls
+{
+    public class MainPanel : Panel
+    {
+        public MainPanel()
+        {
+            Name = "pnl_main";
+            BackColor = Color.LightGray;
+            Anchor = AnchorStyles.None;
+            Size = new Size(300, 300);
+            Dock = DockStyle.None;
+        }
+    }
+}
---
+++ TD Cellule mutante/Controls/SimulationButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+namespace GraphiqueFromCode.Controls
+{
+    class SimulationButton : Button
+    {
+        public SimulationButton()
+        {
+            Name = "btn_simulation";
+            Text = "Simulation";
+            BackColor = Color.LightGray;
+            ForeColor = Color.Black;
+            Size = new Size(300, 40);
+            Dock = DockStyle.None;
+            Font = new Font("Arial", 14);
+            Cursor = Cursors.Hand;
+            SetStyle(ControlStyles.Selectable, false);
+        }
+    }
+}
