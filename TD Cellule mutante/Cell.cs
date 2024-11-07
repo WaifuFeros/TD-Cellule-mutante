92ec598e0cc9b9a2b012b54ab49ea8127814623d
WaifuFeros <payet.pep@gmail.com>
2024-11-07 15:02:37 +0100

    Creation classe Cell

---
+++ TD Cellule mutante/Cell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD_Cellule_mutante
+{
+    public class Cell
+    {
+        public int size;
+        public Color color;
+        public string genetic;
+
+        public Cell(int size, Color color, string genetic)
+        {
+            this.size = size;
+            this.color = color;
+            this.genetic = genetic;
+        }
+
+        public Cell()
+        {
+
+        }
+
+        public void Mutation()
+        {
+
+        }
+
+        public void UpdateSize()
+        {
+
+        }
+
+        public void UpdateColor()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            // inutile
+            return base.ToString();
+        }
+    }
+}
